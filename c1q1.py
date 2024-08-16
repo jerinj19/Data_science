3c9f287cd35ad244cde3c1fb3808b0dec6f9527e
JERIN J <144670486+jerinj19@users.noreply.github.com>
2024-08-16 16:10:45 +0530

    Add files via upload

---
+++ c1q1.py
@@ -0,0 +1,9 @@
+print("SJC23MCA031 : INDULEKHA P S")
+import numpy as np
+# Create a 3D array of float data type
+# we create a 2x3x4 array (2 planes, each with 3 rows and 4columns)
+array_3d = np.array([[[1.5, 2.6, 3.7, 4.8],[5.1, 6.2, 7.3,8.4],[9.5, 10.6, 11.7, 12.8]],
+                      [[13.0, 14.0, 15.0, 16.0],[17.0,18.0, 19.0, 20.0],[21.0, 22.0, 23.0, 24.0]] ], dtype=float)
+# Print the 3D array
+print("3D Array:")
+print(array_3d)
